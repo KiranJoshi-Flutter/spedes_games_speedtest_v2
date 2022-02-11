0155b9eeacfe6afc63bddc3f2588594589930dd2
rajprince65 <rajshah6541@gmail.com>
2022-02-11 09:49:52 +0545

    Raj code

--- lib/GameScreen.dart
+++ lib/GameScreen.dart
@@ -16,8 +16,6 @@ class _GameScreenState extends State<GameScreen> {
   Color? brightBlue = Colors.blue[800];
   Color? brightRed = Colors.red[900];
 
-  late int i;
-
   int randomNumberGenerator() {
     delayColor();
     int randomNumber = Random().nextInt(4);
@@ -25,11 +23,9 @@ class _GameScreenState extends State<GameScreen> {
     return randomNumber;
   }
 
-  void delayColor() {
-    sleep(Duration(seconds: 1));
+  void orginalColor() {
     setState(() {
-      print('------------');
-      print('I am here');
+      print('I am here original color');
       brightYellow = Colors.yellow[700];
       brightGreen = Colors.green[800];
       brightBlue = Colors.blue[800];
@@ -37,29 +33,37 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
+  void delayColor() {
+    sleep(Duration(seconds: 1));
+    print("delayed for 1 second");
+    orginalColor();
+    print("..................................");
+    print("color changed to orgional color");
+  }
+
   void brightColorSelector() {
     int randomValue = randomNumberGenerator();
-    if (randomValue == 0) {
-      setState(() {
-        print('I am Yellow');
-        brightYellow = Colors.yellow;
-      });
-    } else if (randomValue == 1) {
-      setState(() {
-        print('I am Green');
-        brightGreen = Colors.lightGreen;
-      });
-    } else if (randomValue == 2) {
-      setState(() {
-        print('I am Blue');
-        brightBlue = Colors.blue;
-      });
-    } else if (randomValue == 3) {
-      setState(() {
-        print('I am Red');
-        brightRed = Colors.redAccent;
-      });
-    }
+    setState(() {
+      if (randomValue == 0) {
+        print('I am bright Yellow');
+        brightYellow = Colors.black;
+        // delayColor()
+      } else if (randomValue == 1) {
+        print('I am bright Green');
+        brightGreen = Colors.black;
+        // delayColor();
+      } else if (randomValue == 2) {
+        print('I am bright Blue');
+        brightBlue = Colors.black;
+        // delayColor();
+      } else if (randomValue == 3) {
+        print('I am bright Red');
+        brightRed = Colors.black;
+        // delayColor();
+
+      }
+    });
+
     // setState(() {
     //   if (randomNumberGenerator() == 0) {
     //     brightYellow = Colors.yellow;
@@ -73,16 +77,17 @@ class _GameScreenState extends State<GameScreen> {
     // });
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    // brightColorSelector();
-    // randomNumberGenerator();
-  }
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   // brightColorSelector();
+  //   // randomNumberGenerator();
+  // }
 
   @override
   Widget build(BuildContext context) {
+    // delayColor();
     return Scaffold(
       body: Center(
         child: Column(
@@ -94,7 +99,7 @@ class _GameScreenState extends State<GameScreen> {
                 GestureDetector(
                   onTap: () {
                     // randomNumberGenerator();
-                    brightColorSelector();
+                    // brightColorSelector();
                   },
                   child: Container(
                     height: 100.0,
@@ -145,6 +150,31 @@ class _GameScreenState extends State<GameScreen> {
                   ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            GestureDetector(
+              onTap: () {
+                // randomNumberGenerator();
+                brightColorSelector();
+              },
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Loop',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
